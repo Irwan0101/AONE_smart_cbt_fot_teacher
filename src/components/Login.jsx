dac7a3bc2ae0e564b386f6373de1f057eb748bd6
agisfathonah0-lang <agisfathonah0@gmail.com>
2026-05-02 22:39:54 +0700

    change logo

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { supabase } from '../lib/supabase';
 import Swal from 'sweetalert2';
-import logo from '../assets/ONE.png';
+import logo from '../assets/logo.jpg';
 
 const Login = ({ setSession }) => {
     const [token, setToken] = useState('');
